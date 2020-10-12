44cf38e47aa2bd8d331d892a0e3c7a9e3d13dddb
Steven Espinal <stevenjesusespinal@gmail.com>
2020-10-12 17:14:34 -0400

    Added backend routes and custom middleware to express

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import colors from "colors";
 const port = process.env.PORT || 5000;
 import connectDb from "./config/db.js";
+import product from "./routes/product.js";
+import {errorHandler, notFound} from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-
 connectDb();
+
 const app = express();
+
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", product);
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
-});
 
 app.listen(port, () => console.log(`Server is running in ${process.env.NODE_ENV} on port ${port}`.yellow.bold));
