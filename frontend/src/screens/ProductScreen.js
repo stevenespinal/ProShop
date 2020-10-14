5ab6fb95ba6950c2dbd1669c11c0851bc211ba5f
Steven Espinal <stevenjesusespinal@gmail.com>
2020-10-14 00:31:58 -0400

    Added cart route

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,21 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
-import {Row, Col, Image, Button, Card, ListGroup} from "react-bootstrap";
+import {Row, Col, Image, Button, Card, ListGroup, Form} from "react-bootstrap";
 import Rating from "../components/Rating";
 import {detailProducts} from "../actions/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
-const ProductScreen = ({match}) => {
+const ProductScreen = ({match, history}) => {
+  const [qty, setQty] = useState(0);
   const dispatch = useDispatch();
   const productDetail = useSelector((state) => state.productDetail);
   const {loading, error, product} = productDetail;
+  const addToCart = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  }
+
   useEffect(() => {
     dispatch(detailProducts(match.params.id));
   }, [dispatch, match.params.id]);
@@ -19,7 +24,6 @@ const ProductScreen = ({match}) => {
     <>
       <Link to="/" className="btn btn-dark my-3">Go Back</Link>
       {loading ? <Loader/> : error ? <Message variant="danger">{error}</Message> :
-
         <Row>
           <Col md={6}>
             <Image src={product.image} alt={product.name} fluid/>
@@ -59,8 +63,21 @@ const ProductScreen = ({match}) => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && <ListGroup.Item>
+                  <Row>
+                    <Col>Qty:</Col>
+                    <Col>
+                      <Form.Control as="select" value={qty} onChange={e => setQty(e.target.value)}>
+                        {[...Array(product.countInStock).keys()].map(x => (
+                          <option key={x + 1} value={x + 1}>{x + 1}</option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>}
                 <ListGroup.Item>
-                  <Button className="btn-block" type="button" disabled={product.countInStock === 0}>Add To Cart</Button>
+                  <Button onClick={addToCart} className="btn-block" type="button" disabled={product.countInStock === 0}>Add
+                    To Cart</Button>
                 </ListGroup.Item>
               </ListGroup>
             </Card>
