4d8143a605cbc1b0880d58cf54a26848086f2315
Steven Espinal <stevenjesusespinal@gmail.com>
2020-10-17 08:27:15 -0400

    Backend complete for User registration

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,8 +20,36 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+export const registerUser = asyncHandler(async (req, res) => {
+  const {email, password, name} = req.body;
+  const userExists = await User.findOne({email});
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists.");
+  }
+
+  const newUser = await User.create({
+    name,
+    email,
+    password
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: generateToken(newUser._id)
+    })
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data.");
+  }
+
+});
+
 export const getUserProfile = asyncHandler(async (req, res) => {
-  // res.send("/USser");
   const user = await User.findById(req.user._id);
 
   if (user) {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,20 @@
 import express from "express";
-import {authUser, getUserProfile} from "../controllers/user.js";
+import {authUser, getUserProfile, registerUser} from "../controllers/user.js";
 import {protect} from "../middleware/authMiddleware.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
+router.route("/").get(async (req, res) => {
+  const user = await User.find({})
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("No users found.");
+  }
+})
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
