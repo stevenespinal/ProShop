9d9955a372e53ae93d837aeed267e5714ed29619
Steven Espinal <stevenjesusespinal@gmail.com>
2020-10-19 05:11:42 -0400

    Conditionally rendered navigation

--- frontend/src/actions/User.js
+++ frontend/src/actions/User.js
@@ -1,4 +1,4 @@
-import {USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS} from "../types";
+import {USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT} from "../types";
 import axios from "axios";
 
 export const login = (email, password) => async dispatch => {
@@ -18,4 +18,9 @@ export const login = (email, password) => async dispatch => {
       error: error.response && error.response.data.message ? error.response.data.message : error.message
     });
   }
+}
+
+export const logout = () => dispatch => {
+  localStorage.removeItem("userInfo");
+  dispatch({type: USER_LOGOUT});
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
-import {Navbar, Nav, Container} from "react-bootstrap";
-import {LinkContainer} from "react-router-bootstrap"
+import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../actions/User";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector(state => state.userLogin);
+  const {userInfo} = userLogin;
+
+  const logOutHandler = () => {
+    dispatch(logout());
+  }
+
   return (
     <header>
       <Navbar variant="dark" bg="dark" collapseOnSelect expand="lg">
@@ -16,9 +26,16 @@ const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link>Cart <i className="fas fa-shopping-cart"/></Nav.Link>
               </LinkContainer>
+              {userInfo ? <NavDropdown title={userInfo.name} id="username">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logOutHandler}>Log Out</NavDropdown.Item>
+                </NavDropdown> :
               <LinkContainer to="/login">
                 <Nav.Link>Sign In <i className="fas fa-user"/></Nav.Link>
               </LinkContainer>
+                }
             </Nav>
           </Navbar.Collapse>
         </Container>
